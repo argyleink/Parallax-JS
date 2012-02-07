fcc41a670e03d8bbcd752c51ab9935f5f2dcd5cf
markkahn <markkahn@1160f83d-d3b9-402c-ae07-89e89a204893>
2012-02-07 22:04:01 +0000

    Working on parallax
    git-svn-id: https://aarf.svn.cvsdude.com/rfbdthankyou/branches/parallax@74 1160f83d-d3b9-402c-ae07-89e89a204893

--- parallax.js
+++ parallax.js
@@ -1,52 +1,63 @@
 // location.replace( '#id' ) -- changes hash URI without history state
 
 $(function(){
-	var animations = []
-	  ,  $animTags = $( 'animation' )
-	  ,  $scroller = $( '#mock-scroller' )
-	  ,   $content = $( '#content' )
-	  ,  ttlHeight = 0
-	  , i, l, j, m, anim, attr, windowHeight, $anim;
+	var     $window = $( window )
+	  ,       $body = $( 'body' )
+	  ,    $content = $( '#content' )
+	  ,   $sections = $content.find( 'section' )
+	  ,   $scroller = $( '#mock-scroller' )
+	  , fScrPercent = 0
+	  ,  aAnimProps = [ 'opacity', 'left', 'top', 'background-position' ]
+	  , iAnimTimeout, iWindowHeight, aAnimations
+	  ;
 
-	for( i=0, l=$animTags.length; i<l; i++ ){
-		$anim = $( $animTags[i] );
-		anim  = {
-			     from : {}
-			,      to : {}
-			, domNode : $anim.parent()
-			, thisAnim: $animTags[i]
-		};
+	// find all animatable nodes and store properties
+	$sections.each( function( ix ){
+		var $sec = $sections.eq( ix );
+		$sec.data( 'p-nodes', $sec.find( '.animate' ) );
 
-		for( j=0, m=$anim[0].attributes.length; j<m; j++ ){
-			attr = $anim[0].attributes[j].name;
+		$sec.add( $sec.data( 'p-nodes' ) ).each( function(){
+			var $this = $( this )
+			  , oData = $this.data()
+			  ;
 
-			switch( attr ){
-				case 'delay':
-					anim.delay = parseInt( $anim.attr( attr ) );
-					break;
+			oData.iPause    = 0 | $this.attr( 'anim-pause' );
+			oData.bDetached = !!~'1 true'.indexOf( $this.attr( 'anim-detached' ) );
+			oData.onFocus   = $this.attr( 'anim-focus-handler' );
+			oData.onBlur    = $this.attr( 'anim-blur-handler' );
+		} );
 
-				default:
-					anim.to  [ attr ] = $anim.attr( attr );
-					anim.from[ attr ] = anim.domNode.css( attr );
-					break;
-			}
-		}
+		// remove the section from the DOM
+		$sec.detach();
+	} );
 
-		animations.push( anim );
+	// window loaded or re-sized, re-calculate all dimensions
+	function measureAnimations(){
+		var iTop = 0;
+		aAnimations = [];
+
+		$sections.each( function( ix ){
+			var       $sec = $( this )
+			  ,      oData = $sec.data()
+			  , iSecHeight = 0
+			  ;
+
+			oData.startsAt = iTop;
+
+			// append section to content and reset position
+			$sec
+				.css({ top : 0 })
+				.appendTo( $content );
+
+			iSecHeight += $sec.outerHeight();
+
+			$sec.detach();
+
+			oData.endsAt = iTop += iSecHeight;
+		} );
 	}
 
-	function reCalcHeight(){
-		ttlHeight = 0;
-		for( i=0, l=animations.length; i<l; i++ ){
-			anim = animations[i];
-
-			anim.top    = ttlHeight;
-			anim.height = parseInt( anim.domNode.outerHeight() );
-			ttlHeight  += anim.height + ( anim.delay || 0 );
-		}
-	}
-
-	$( window )
+	$window
 		/**
 		 * On resize:
 		 * 
@@ -55,51 +66,15 @@ $(function(){
 		 * - adjust top position so that it's at the same %, not same px
 		 **/
 		.bind( 'resize', function(){
-			windowHeight = $( window ).height();
-			reCalcHeight();
+			if( iAnimTimeout ){ clearTimeout( iAnimTimeout ); }
+			iAnimTimeout = setTimeout( measureAnimations, 50 );
+
+			iWindowHeight = $window.height();
 		})
 		.trigger( 'resize' );
 
 	$scroller
 		.bind( 'scroll', function( ev ){
 			var scrTop = $scroller.scrollTop();
-
-/*
-on scroll:
-
-loop each element that has any animation
-- if element is on screen ( top < window.scrollTop + window.height ) && ( bottom > window.scrollTop )
-	- set .css() of the item to ( end - start ) * percentScrolledOfItem
- */
-
-			for (var i=0, l=animations.length; i<l; i++ ){
-				var       anim = animations[i]
-				  ,     value1 = ( anim.top + windowHeight )
-				  ,     value2 = ( anim.top - anim.height )
-				  , scrPercent = ( scrTop - value2 ) / ( value1 - value2 );
-
-				if ( ( anim.top < ( scrTop + windowHeight ) ) && ( ( anim.top + anim.height ) > scrTop ) ) {
-					// determine % between ( anim.top + windowHeight ) & ( anim.top - anim.height )
-					//                     ( 1000     + 500          )   ( 1000     - 500         )
-					//                     1500 -- 500
-					//                     % = ( 1200 - 500 ) / ( 1500 - 500 )
-					//                     % = ( 700 / 1000 ) = .7
-					// scrTop = 1200
-					// anim.top = 1000
-					// anim.height = 500
-					// windowHeight = 500
-
-					// create empty object to hold new css properties: {}
-					// for each thing in anim.from
-						// set new object.property = ( to - from ) * percent + from
-
-				var newFrom = {};
-				for( i in anim.from ){
-					newFrom[i] = (anim.to[i] - anim.from[i]) * (scrPercent + anim.from[i]);
-
-					}
-					anim.from = newFrom;
-				}
-			}
 		} );
 });
